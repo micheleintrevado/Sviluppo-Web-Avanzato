f3485f9a00c8ca0e53fb03b5656dfc1e20cd08f4
Michele Intrevado <michele.intrevado@student.univaq.it>
2024-06-12 17:53:25 +0200

    patch

--- src/main/webapp/res/eventi.js
+++ src/main/webapp/res/eventi.js
@@ -10,6 +10,18 @@ $(document).ready(function () {
 
     $('#tipologia_evento').trigger('change');
 
+
+    $('#tipologia_evento_modifica').change(function () {
+        let selectedValue = $('#tipologia_evento_modifica').val();
+        if (selectedValue === 'lezione' || selectedValue === 'esame' || selectedValue === 'parziale') {
+            $('#corso_container_modifica').show();
+        } else {
+            $('#corso_container_modifica').hide();
+        }
+    });
+
+    $('#tipologia_evento_modifica').trigger('change');
+
     $('input[name="ricorrenza_evento"]').change(function () {
         let selectedValue = $('input[name="ricorrenza_evento"]:checked').val();
         console.log(selectedValue);
@@ -72,5 +84,58 @@ function addEvento() {
     });
 }
 
+function modificaEvento() {
+    $.ajax({
+        url: "rest/eventi/" + $('#id_evento_modifica').val(),
+        method: "PATCH",
+        contentType: "application/json",
+        data: JSON.stringify({
+            nome: $('#nome_evento_modifica').val(),
+            orario_inizio: new Date($('#orario_inizio_modifica').val()).toISOString(),
+            orario_fine: new Date($('#orario_fine_modifica').val()).toISOString(),
+            descrizione: $('#descrizione_evento_modifica').val(),
+            nome_organizzatore: $('#nome_organizzatore_evento_modifica').val(),
+            email_responsabile: $('#email_responsabile_evento_modifica').val(),
+            tipologia: $('#tipologia_evento_modifica').val(),
+            id_aula: parseInt($('#id_aula_evento_modifica').val()),
+            id_corso: parseInt($('#id_corso_evento_modifica').val())
+        }),
+        success: function (request, status, error) {
+            // header.substring("Bearer".length).trim();
+            alert("modificaEvento ok");
+            console.log("modificaEvento ok");
+        },
+        error: function (request, status, error) {
+            if (request.status == 401) alert("LOGIN ERROR");
+            console.log("modificaEvento error: " + request.status);
+        },
+        // function (request, status, error) {
+        //     handleError(request, status, error, "#collezione", "Errore nel caricamento della collezione.");
+        // },
+        cache: false
+    });
+}
+
+function getEventiUtility() {
+    $.ajax({
+        url: "rest/eventi/",
+        method: "GET",
+        success: function (data) {
+            $.each(data, function (key) {
+                $("[name='lista_id_eventi']").append(
+                    "<option value=" + data[key] + ">" + data[key] + "</option>")
+            });
+        },
+        error: function (request, status, error) {
+            alert("ID EVENTI NON TROVATI");
+            console.log("ID EVENTI NON TROVATI");
+        },
+        cache: false
+    });
+}
+
+getEventiUtility();
 const addEventoBtn = $('#addEvento_button');
 addEventoBtn.click(addEvento);
+const modificaEventoBtn = $('#modificaEvento_button');
+modificaEventoBtn.click(modificaEvento);
